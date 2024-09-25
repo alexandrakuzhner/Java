5ca2b0973e7e045fa9f8498cc00faf7638900e31
IMT-Teilnehmer <kuzhnersasha@gmail.com>
2024-09-25 23:40:43 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        char one = 'G';
+        int two = 89;
+        byte tree = 4;
+        short four = 56;
+        float fife = 4.7333436f;
+        double six = 4.355453532;
+        long seven = 12121L;
+
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(tree);
+        System.out.println(four);
+        System.out.println(fife);
+        System.out.println(six);
+        System.out.println(seven);
+
+        Character character = 'G';
+        Integer integer = 89;
+        Byte byteValue = 4;
+        Short shortValue = 56;
+        Float floatValue = 4.7333436f;
+        Double doubleValue = 4.355453532;
+        Long longValue = 12121L;
+
+        System.out.println(character);
+        System.out.println(integer);
+        System.out.println(byteValue);
+        System.out.println(shortValue);
+        System.out.println(floatValue);
+        System.out.println(doubleValue);
+        System.out.println(longValue);
+
+        //Задание 3
+        int drei = 3;
+        int vier = 4;
+        int fünf = 5;
+
+        System.out.println("Число 345 -> " + drei + ',' + vier + ',' + fünf);
+
+    }
+}
